b6c95f00f0a2502cfa51b9ffba5b5a2706b26bfe
GLADCHENKO Andrey O <Andrey.GLADCHENKO@raiffeisen.ru>
2024-03-25 11:23:56 +0400

    removed debugger from app

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -76,7 +76,3 @@ def create_url_check(ind: int):
 @app.errorhandler(404)
 def page_not_found():
     return render_template("errors/404.html"), 404
-
-
-if __name__ == "__main__":
-    app.run(debug=True, port=8000)
